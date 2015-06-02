fdd4ef81f7659a234b101213695614966d799fc2
ChefDK User <chef@ip-172-31-13-83.us-west-1.compute.internal>
2015-06-02 22:54:08 +0000

    added additional tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,16 @@ describe 'workstation::default' do
   describe package('tree') do
     it { should be_installed }
   end
+
+  describe package('nano') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/motd') do
+    it { should contain 'Property' }
+  end
+
+  describe command('ls -la /') do
+    its(:stdout) { should match /bin/ }
+  end
 end
